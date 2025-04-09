02cbcbb258dd607bb190b37ccade36c90428a83d
cure23 <cure23xv@student.ju.se>
2025-04-09 13:26:30 +0200

    multiple js files (main loads data, others create and render content)

---
+++ js/browse.js
@@ -0,0 +1,41 @@
+const contentElement = document.getElementById("content");
+
+function createMovieElement(movie) {
+  const movieLinkElement = document.createElement("a");
+  movieLinkElement.classList.add("link");
+  movieLinkElement.href = "detail.html?movie=" + movie.id;
+
+  const movieElement = document.createElement("article");
+  movieElement.classList.add("movie");
+  movieLinkElement.appendChild(movieElement);
+
+  const imageElement = document.createElement("img");
+  imageElement.classList.add("padding");
+  imageElement.src = movie.posterImg;
+  movieElement.appendChild(imageElement);
+
+  const infoElement = document.createElement("div");
+  infoElement.classList.add("padding", "info");
+  movieElement.appendChild(infoElement);
+
+  const titleElement = document.createElement("h4");
+  titleElement.classList.add("text");
+  titleElement.innerText = movie.title;
+  infoElement.appendChild(titleElement);
+
+  const scoreElement = document.createElement("p");
+  scoreElement.classList.add("text");
+  scoreElement.innerText = starScore(movie.score).join(" ");
+  infoElement.appendChild(scoreElement);
+
+  return movieLinkElement;
+}
+
+function renderContent() {
+  contentElement.innerHTML = ""; //empty everything
+
+  for (let movie of movies) {
+    const movieElement = createMovieElement(movie);
+    contentElement.appendChild(movieElement);
+  }
+}
---
+++ js/detail.js
@@ -0,0 +1,72 @@
+const contentElement = document.getElementById("d-content");
+
+function renderContent() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get("movie");
+  /* console.log(movieId + 2); */
+  const movie = getMovieById(Number(movieId));
+
+  contentElement.innerHTML = "";
+
+  const detailElement = createDetailElement(movie);
+  contentElement.appendChild(detailElement);
+
+  const movieReviews = getAllReviewsOf(movie);
+  for (let movieReview of movieReviews) {
+    const reviewElement = createReviewElement(movieReview);
+    contentElement.appendChild(reviewElement);
+  }
+}
+
+function createDetailElement(movie) {
+  const detailElement = document.createElement("section");
+  detailElement.classList.add("d-movie");
+
+  const imageElement = document.createElement("img");
+  imageElement.classList.add("padding", "poster");
+  imageElement.src = movie.posterImg;
+  detailElement.appendChild(imageElement);
+
+  const infoElement = document.createElement("div");
+  infoElement.classList.add("padding", "info");
+  detailElement.appendChild(infoElement);
+
+  const titleElement = document.createElement("h4");
+  titleElement.classList.add("text");
+  titleElement.innerText = movie.title;
+  infoElement.appendChild(titleElement);
+
+  const releaseElement = document.createElement("h6");
+  releaseElement.classList.add("text", "stars");
+  releaseElement.innerText = movie.release;
+  infoElement.appendChild(releaseElement);
+
+  const scoreElement = document.createElement("p");
+  scoreElement.classList.add("text");
+  scoreElement.innerText = starScore(movie.score).join(" ");
+  infoElement.appendChild(scoreElement);
+
+  const reviewByElement = document.createElement("p");
+  reviewByElement.classList.add("text", "reviewed-by");
+  reviewByElement.innerText =
+    "reviewed by " + getUserById(getReviewByMovie(movie).userId).name;
+  infoElement.appendChild(reviewByElement);
+
+  return detailElement;
+}
+
+function createReviewElement(review) {
+  const reviewElement = document.createElement("article");
+  /* detailElement.classList.add("d-movie"); */
+
+  const textElement = document.createElement("p");
+  textElement.innerText = review.text;
+  reviewElement.appendChild(textElement);
+
+  const scoreElement = document.createElement("p");
+  scoreElement.classList.add("text");
+  scoreElement.innerText = starScore(review.score).join(" ");
+  reviewElement.appendChild(scoreElement);
+
+  return reviewElement;
+}
